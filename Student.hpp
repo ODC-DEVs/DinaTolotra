9f8fe99c9f3b6241987b7bd1b3dc8eba06b4e113
DinaTolotra <RatsinjomananaTolotra@gmail.com>
2024-09-20 15:29:18 +0300

    Adding comment in Student class

--- Student.hpp
+++ Student.hpp
@@ -16,16 +16,44 @@ private:
 public:
     Student(std::string name, std::string firstName, int registrationNumber);
 
+    /**
+     * @brief Insérer un note dans le liste de note
+     * @param score: Le note à insérer dans le liste
+     */
     void addScore(float score);
 
+    /**
+     * @brief Calculer la moyenne des notes enregistrées
+     * @return La moyenne calculer
+     */
     float calculateAverage();
+    /**
+     * @brief Calculer le médiane du liste de note
+     * @return Le médiane du liste de note
+     */
     float calculateMedian();
 
+    /**
+     * @brief Obtenir le meilleur note de l'etudiant
+     * @return Le meilleur note enregistrée de l'etudiant
+     */
     float getBestScore();
+    /**
+     * @brief Obtenir le pire note de l'etudiant
+     * @return Le pire note enregistrée de l'etudiant
+     */
     float getWorstScore();
 
+    /**
+     * @brief Trie les note enregistrée de manière croissante
+     * @return Le liste de note trié
+     */
     std::vector<float> sortNote();
 
+    /**
+     * @brief Verifie si l'étudiant est admis ou non
+     * @return True si l'étudiant est admis, False sinon
+     */
     bool isAccepted();
 };
 
